ff3679c99a59bca8fd6970bcb0216d69127f1fc4
peter <peterleed01@gmail.com>
2025-01-07 22:30:16 +0100

    adjusted the theme color and made it responsive for mobile

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
-const input = document.getElementById('input');
-const button = document.getElementById('button');
-const list = document.getElementById('list');
-const remove = document.getElementById('remove');
-const error = document.getElementById('message');
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const list = document.getElementById("list");
+const remove = document.getElementById("remove");
+const error = document.getElementById("message");
 
-function appendTask(task){
-    const li = document.createElement('li');
-    li.innerHTML = task;
+function appendTask(task) {
+  const li = document.createElement("li");
+  li.innerHTML = task;
 
-    const button = document.createElement('button');
-    button.innerText = 'x';
-    
-    li.appendChild(button);
-    list.appendChild(li);
+  const button = document.createElement("button");
+  button.innerText = "❌";
 
-    button.addEventListener('click', ()=>{
-        input.value = task;
-        li.remove();
-    })
+  li.appendChild(button);
+  list.appendChild(li);
 
-    li.addEventListener('click', () => {
-        li.classList.toggle('completed');
+  button.addEventListener("click", () => {
+    input.value = task;
+    li.remove();
+  });
 
-        setTimeout(() => li.remove(), 3000);
-    })
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+
+    // setTimeout(() => li.remove(), 3000);
+  });
 }
 
-button.addEventListener('click', (e)=>{
-    e.preventDefault();
-    if (input.value == ''){
-        error.classList.add('error');
-        error.innerText = `Input can't be empty`;
-        setTimeout(()=> error.innerText='', 3000);
-        setTimeout(()=> error.classList.remove('error'), 3000)
-    } else {
-        appendTask(input.value);
-        input.value = '';
-    }
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input.value == "") {
+    error.classList.add("error");
+    error.innerText = `Input can't be empty`;
+    setTimeout(() => (error.innerText = ""), 3000);
+    setTimeout(() => error.classList.remove("error"), 3000);
+  } else {
+    appendTask(input.value);
+    input.value = "";
+  }
 });
